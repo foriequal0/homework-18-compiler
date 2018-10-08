63d2e46bc35982dfb8ac937a8f4e6cca0378c310
SeongChan Lee <foriequal@gmail.com>
2018-10-09 03:59:08 +0900

    Add some comments

--- skeleton/hash.c
+++ skeleton/hash.c
@@ -18,33 +18,38 @@ typedef struct nlist {
 
 static nlist *hashTable[HASH_TABLE_SIZE];
 
+/* Java string hash algorithm
+ * sum i=0..n (s[i] * 31 ^ (n-1-i)) */
 unsigned hash(const char *name) {
+  /* Unsigned integer overflow is well defined behaviour,
+   * while signed one is undefined behaviour */
 	unsigned h = 0;
 	for(const char *p = name; *p; p++) {
-	    h = (h * 31) + *p;
+    h = (h * 31) + *p;
 	}
 	return h;
 }
 
 id *enter(int tokenType, const char *name, int length) {
-    unsigned h = hash(name) % HASH_TABLE_SIZE;
+  unsigned h = hash(name) % HASH_TABLE_SIZE;
 
-    for(nlist* entry = hashTable[h];entry; entry=entry->next) {
-        if (strncmp(entry->data->name, name, length) == 0) {
-            return entry->data;
-        }
+  /* Find existing entry, and exit early if it is found */
+  for(nlist* entry = hashTable[h];entry; entry=entry->next) {
+    if (strncmp(entry->data->name, name, length) == 0) {
+      return entry->data;
     }
+  }
 
-    id* data = (id*)malloc(sizeof(id));
-    char *name_copy = (char*)malloc(strnlen(name, length)+1);
-    data->name = strncpy(name_copy, name, length);
-    data->tokenType = tokenType;
-    data->count = 0;
+  id* data = (id*)malloc(sizeof(id));
+  char *name_copy = (char*)malloc(strnlen(name, length)+1);
+  data->name = strncpy(name_copy, name, length);
+  data->tokenType = tokenType;
+  data->count = 0;
 
-    nlist* entry = (nlist*)malloc(sizeof(nlist));
-    entry->data = data;
-    entry->next = hashTable[h];
-
-    hashTable[h] = entry;
-    return data;
+  /* Maintain linked list for collided entries */
+  nlist* entry = (nlist*)malloc(sizeof(nlist));
+  entry->data = data;
+  entry->next = hashTable[h];
+  hashTable[h] = entry;
+  return data;
 }
