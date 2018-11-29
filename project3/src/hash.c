42b3e03d2ba9ac426a4153026bdadd3a70e9a731
SeongChan Lee <foriequal@gmail.com>
2018-11-30 05:12:03 +0900

    Bare syntax definitions

--- project3/src/hash.c
+++ project3/src/hash.c
@@ -6,6 +6,8 @@
  ****************************************************************/
 
 #include "subc.h"
+#include <string.h>
+#include <stdlib.h>
 
 #define  HASH_TABLE_SIZE   101
 
@@ -16,15 +18,43 @@ struct nlist {
 
 static struct nlist *hashTable[HASH_TABLE_SIZE];
 
-unsigned hash(char *name) {
-   /* implementation is given here */
+/* Java string hash algorithm
+ * sum i=0..n (s[i] * 31 ^ (n-1-i)) */
+unsigned hash(const char *name) {
+  unsigned h = 0;
+  for(const char *p = name; *p; p++) {
+    h = (h * 31) + *p;
+  }
+  return h;
 }
 
-struct id *enter(int lextype, char *name, int length) {
-   /* implementation is given here */
+struct id *enter(int lextype, const char *name) {
+  unsigned h = hash(name) % HASH_TABLE_SIZE;
+
+  /* Find existing entry, and exit early if it is found */
+  struct id* existing = lookup(name);
+  if (existing) {
+    return existing;
+  }
+
+  struct id* data = (struct id*) calloc(1, sizeof(struct id));
+  data->name = strdup(name); // copy yytext because it is modified while parsing.
+  data->lextype = lextype;
+
+  struct nlist* entry = (struct nlist*) malloc(sizeof(struct nlist));
+  entry->data = data;
+  entry->next = hashTable[h];
+  hashTable[h] = entry;
+  return data;
 }
 
-struct id *lookup(char *name) {
-   /* implementation is given here */
+struct id *lookup(const char *name) {
+  unsigned h = hash(name) % HASH_TABLE_SIZE;
+  for (struct nlist* entry = hashTable[h]; entry; entry = entry->next) {
+    if (strcmp(entry->data->name, name) == 0) {
+      return entry->data;
+    }
+  }
+  return NULL;
 }
 
